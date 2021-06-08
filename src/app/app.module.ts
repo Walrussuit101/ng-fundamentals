fd6a4f68ffe770dea03b01dcb22c584cd1d07410
Walrussuit101 <tjefferson@ycp.edu>
2021-06-08 14:56:14 -0400

    create an event service
    also first time using @Injectable and ngOnInit()

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
+import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
