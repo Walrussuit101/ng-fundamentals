1e20d775d611193f4aec868204c09b06035f2a0c
Walrussuit101 <tjefferson@ycp.edu>
2021-06-09 13:15:56 -0400

    add event creation functionality
    also explore more concepts with template driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,13 @@ import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { Error404Component } from './errors/404.component';
 import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   declarations: [
