bce448e0f5b807143d5cb67977aa8f1f459bc735
Walrussuit101 <tjefferson@ycp.edu>
2021-06-09 09:03:20 -0400

    orgnanize event imports using barrels concept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { RouterModule } from '@angular/router';
 import { ToastrService } from './common/toastr.service';
 import { appRoutes } from './routes';
 
+import {
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  EventsListComponent,
+  EventService,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index';
+
 import { EventsAppComponent } from './events-app.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/navbar.component';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/event-list-resolver.service';
 
 @NgModule({
   imports: [
