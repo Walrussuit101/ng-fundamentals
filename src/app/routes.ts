60a7c322cbf3a5d389d4c62cf51c38c9f923945c
Walrussuit101 <tjefferson@ycp.edu>
2021-06-09 08:55:57 -0400

    add user feature module
    define the module to be lazy loaded for routes /user/*

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -11,5 +11,12 @@ export const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
     { path: '404', component: Error404Component },
-    { path: '', redirectTo: '/events', pathMatch: 'full' }
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+
+    //user feature module
+    { 
+        path: 'user', 
+        loadChildren: () => import('./user/user.module')
+            .then(m => m.UserModule)
+    }
 ]
